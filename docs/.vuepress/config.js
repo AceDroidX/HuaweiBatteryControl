d8a0c7a139b2b47f3919494ce92689b00e2cd1b3
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2023-06-03 01:54:16 +0800

    Use VuePress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    base: '/HuaweiBatteryControl/',
+    lang: 'zh-CN',
+    title: 'HuaweiBatteryControl',
+    description: '华为笔记本充电控制逆向过程记录',
+})
